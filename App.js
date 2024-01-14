4cd81098ca41acc5671b17b32e143893e662725c
Temkhu <teemu.ritatorma@gmail.com>
2024-01-14 19:58:03 +0200

    Update App.js

--- App.js
+++ App.js
@@ -1,10 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+
+  const [age, setAge] = useState('');
+  const [up, setUp] = useState('');
+  const [low, setLow] = useState('');
+
+    const hr = () => {
+      result1 = Math.round((220 - age.replace(',','.')) * 0.65);
+      result2 = Math.round((220 - age.replace(',','.')) * 0.85);
+
+      setUp(result2);
+      setLow(result1);
+    }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.field}>Age</Text>
+      <TextInput
+      style={styles.reuna}
+      keyboardType='number-pad'
+      value={age}
+      onChangeText={text => setAge(text)}
+      onSubmitEditing={hr}
+      />
+      <Text style={styles.field}>Hr Limit</Text>
+      <Text style={styles.field}>{low} - {up}</Text>
+      {/* <Button title ="hr" onPress={hr}></Button> */}
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +38,18 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    padding: 8,
   },
+  field: {
+    marginBottom: 8,
+  },
+  reuna:{
+    borderColor: 'lightblue',
+    borderWidth: 3,
+    borderRadius: 2,
+    padding: 2,
+    marginBottom: 2,
+    paddingLeft: 4,
+  }
 });
